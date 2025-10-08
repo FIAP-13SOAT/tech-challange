d2294d41450cc64503bd9d28fd3ac2e400a682ff
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2025-10-08 17:37:41 -0300

    refactor: movendo DTO

--- src/main/java/com/fiapchallenge/garage/adapters/inbound/controller/servicetype/dto/ServiceTypeRequestDTO.java
+++ src/main/java/com/fiapchallenge/garage/adapters/inbound/controller/servicetype/dto/ServiceTypeRequestDTO.java
@@ -1,4 +1,4 @@
-package com.fiapchallenge.garage.domain.servicetype;
+package com.fiapchallenge.garage.adapters.inbound.controller.servicetype.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
