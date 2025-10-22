1f53b003ae01ae144991b95927009bb07da35e36
Johann Bandelow <johannabandelow@gmail.com>
2025-10-21 21:42:25 -0300

    imports

--- src/main/java/com/fiapchallenge/garage/adapters/outbound/repositories/customer/CustomerRepositoryImpl.java
+++ src/main/java/com/fiapchallenge/garage/adapters/outbound/repositories/customer/CustomerRepositoryImpl.java
@@ -8,10 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Component
 public class CustomerRepositoryImpl implements CustomerRepository {
