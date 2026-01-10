5a89fa404582570325445fb9318751c8545e0b64
João Pedro Truchinski Borba <joao.borba@asaas.com.br>
2026-01-10 18:14:56 -0300

    fix: cluster deve responder apenas por IP publico

--- infra/main.tf
+++ infra/main.tf
@@ -237,7 +237,7 @@ resource "aws_eks_cluster" "eks_cluster" {
             aws_security_group.main.id
         ]
         endpoint_public_access  = true
-        endpoint_private_access = true
+        endpoint_private_access = false
     }
 
     access_config {
