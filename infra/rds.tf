dd2f231ac79732d9a7d3288a5152ec431f7aa694
nuneserick <erick.nunes@asaas.com.br>
2026-01-20 08:09:35 -0300

    fix: Definindo senha fixa

--- infra/rds.tf
+++ infra/rds.tf
@@ -15,7 +15,7 @@ resource "aws_db_instance" "postgres" {
 
     db_name  = "garage"
     username = "postgres"
-    manage_master_user_password = true
+    password = "fYB#kbp(_#AIf3J7ASYboH..RG34"
 
     vpc_security_group_ids = [aws_security_group.rds.id]
     db_subnet_group_name   = aws_db_subnet_group.main.name
